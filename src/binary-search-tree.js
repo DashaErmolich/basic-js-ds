67349ce3e14f684b8d97924b66e87097fe6703e7
DashaErmolich <dasha.ermolich@gmail.com>
2022-10-11 14:04:18 +0200

    feat: BST task solved

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,164 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.rootData = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootData) {
+      return null;
+    } else {
+      return this.rootData;
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootData = addData(this.rootData, data)
+
+    function addData(node, data) {
+      if(!node) {
+        return new Node(data);
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if(node.data < data) {
+        node.right = addData(node.right, data);
+      }
+
+      if (node.data > data) {
+        node.left = addData(node.left, data);
+      }
+
+      return node;
+    }
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return isHasData(this.rootData, data);
+
+    function isHasData(node, data) {
+      if (!node) {
+        return false;
+      }
+
+      if (node.data === data) {
+        return true;
+      }
+
+      if (node.data < data) {
+        return isHasData(node.right, data);
+      }
+
+      if (node.data > data) {
+        return isHasData(node.left, data);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findData(this.rootData, data);
+
+    function findData(node, data) {
+      if (!node) {
+        return null;
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (node.data < data) {
+        return findData(node.right, data)
+      } else {
+        return findData(node.left, data)
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.rootData = removeData(this.rootData, data);
+
+    function removeData(node, data) {
+      if (!node) {
+        return undefined;
+      }
+
+      if (node.data < data) {
+        node.right = removeData(node.right, data);
+        return node;
+      } else if (node.data > data) {
+        node.left = removeData(node.left, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minNodeRightData = node.right;
+
+        while (minNodeRightData.left) {
+          minNodeRightData = minNodeRightData.left;
+        }
+
+        node.data = minNodeRightData.data;
+        node.right = removeData(node.right, minNodeRightData.data);
+
+        return node;
+      }
+
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (!this.rootData) {
+      return null;
+    }
+
+    let node = this.rootData;
+
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (!this.rootData) {
+      return null;
+    }
+
+    let node = this.rootData;
+
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.data;
   }
 }
 
